961d74aeb7172db83674d64e34149fa809f19b0f
shanekao <El3vul4cl3gj94>
2022-01-03 11:36:37 +0800

    Style: remove redundant

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ except ImportError:
     from distutils.core import setup
 
 
-setup(
-    name='mimi',
-    version='1.2a-beta',
-    packages=['mimi'],
-    # py_modules=['mimi','mode','output','preprocess','generator'],  # 將模組的metadata關連到setup函式
-    author='exeex',
-    author_email='xray0h@gmail.com',
-    url='....',
-    description='beta',
-)
+# setup(
+#     name='mimi',
+#     version='1.2a-beta',
+#     packages=['mimi'],
+#     # py_modules=['mimi','mode','output','preprocess','generator'],  # 將模組的metadata關連到setup函式
+#     author='exeex',
+#     author_email='xray0h@gmail.com',
+#     url='....',
+#     description='beta',
+# )
 
 here = os.path.abspath(os.path.dirname(__file__))
 
